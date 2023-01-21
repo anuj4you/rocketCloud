84c19bcff47611f1e1b1223630b3d1f3321443e8
Anuj <anujk.581@gmail.com>
2023-01-21 04:45:57 -0500

    final initial with all files and images

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div><h1>I am About Us component</h1></div>
+  )
+}
---
+++ src/components/Platform.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Platform() {
+  return (
+    <div>I am Platform Component</div>
+  )
+}
